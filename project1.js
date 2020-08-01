d24f2625b7bc302386c6bbddffdb888679fa8791
alex yanovich <alexyanovich@hotmail.com>
2020-08-01 16:16:12 -0400

    started converting pseodo code to real code

--- project1.js
+++ project1.js
@@ -1,72 +1,96 @@
-// let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q, A ];
-// let suits = [hearts, diamonds, spades, clubs];
-// let 
-// class GameRules {
-//      constructor() {
-//           this.maxValue = 21
-//           this.roundWin
-//           this.roundlose
+let cardValues = [
+          [2, 2],
+          [3, [3, [4, 4],
+                    [5, 5],
+                    [6, 6],
+                    [7, 7],
+                    [8, 8],
+                    [9, 9],
+                    [10, 10],
+                    [j, 10],
+                    [K, K],
+                    [Q, 10],
+                    [A, [1, 11]]
+                    let suits = [hearts, diamonds, spades, clubs];
+                    let
+                         class GameRules {
+                              constructor() {
+                                   this.maxValue = 21
+                                   this.roundWin
+                                   this.roundlose
 
-//      }
-//      checkWin(){
-//           if (playerCardValue >= dealerCardValue && playerCardValue >= 21);{
-//                console.log("you win")
-//           }
-//           checkLose(){
-//                if (playerCardValue < dealerCardValue && dealerCardValue <= 21){
-//                     console.log("you lose");
-//                }
-//           }
+                              }
+                              checkWin() {
+                                   if (playerCardValue >= dealerCardValue && playerCardValue >= 21); {
+                                        console.log("you win")
+                                   }
+                                   checkLose() {
+                                        if (playerCardValue < dealerCardValue && dealerCardValue <= 21) {
+                                             console.log("you lose");
+                                        }
+                                   }
 
-//      }
-// }
+                              }
+                         }
 
-// class player {
-//      constructor(playerCash, playerCardValue, playerBet) {
-//           this.playerCash = 100
-//           this.playerCardValue = null
-//           this.playerBet = null
-//           this.hit = //+ 1 card
-//      }
-//      playerWin(playerCash) += this.playerBet;
-//      }
-//      playerLose(playerCash) -= this.playerBet;
+                    class player {
+                         constructor(playerCash, playerCardValue, playerBet) {
+                              this.playerCash = 100
+                              this.playerCardValue = null
+                              this.playerBet = null
+                              this.hit = //+ 1 card
+                         }
+                         playerWin(playerCash) += this.playerBet;
+                    }
+                    playerLose(playerCash) -= this.playerBet;
 
-// }
-//      class dealer{
-//           constructor() {
-//                this.maxHit = 17
-
-          
-//           }
-//      }
-
-
-
-// // function shuffle()
-// // // use math.randon and math.floor to shuffle the array of cards.
-// create a new variable called deck
-
-// // function startBlackJack()
-// // // begins the game of blackjack
-
-// // dealCards(deck) //jquery
-// if dealButton = clicked display two player carda and two dealer cards via deck function. if pressed again pull another card for player. repeat until player pressed stayButton or goes over 21 //void fucntion
-
-// stayButton()
-// if playerCardValue < 21 && player pushes stayButton dealer hits until dealer card value is > 17
-
-// checkWin()
-
-// if checkWin === true
-// playerBet += playerBank
-
-// check lose === true
-// playerBet -= playerBank
-
-// // function playerBank()
+               }
+               class dealer {
+                    constructor() {
+                         this.maxValue = 18
 
 
-// loop back to beginning of the game but rentain player bank
+                    }
+               }
 
 
+
+               // j query removes card from deck
+               // // function shuffle()
+               // // // use math.randon and math.floor to shuffle the array of cards.
+               // create a new variable called deck
+
+               function startBlackJack()
+               const player = new player();
+               const dealer = new dealer();
+
+               player.playerBet = prompt("enter your bet amount")
+               while (player.playerBet > player.playerCash || player.playeBet.NaN == true) {
+                    alert("you dont have anough money");
+                    player.playerBet = prompt("enter your bet amount");
+               }
+
+               while (player.playerCardValue <= 21 && dealer.maxValue <= 18) {
+                    drawCard()
+
+
+               }
+
+
+               // // dealCards(deck) //jquery
+
+               // stayButton()
+               // if playerCardValue < 21 && player pushes stayButton dealer hits until dealer card value is > 17
+
+               // checkWin()
+
+               // if checkWin === true
+               // playerBet += playerBank
+
+               // check lose === true
+               // playerBet -= playerBank
+
+               // // function playerBank()
+
+
+               // loop back to beginning of the game but rentain player bank
