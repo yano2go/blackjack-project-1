d80516f376ba9fd06da945178a5691fe89c3376b
alex yanovich <alexyanovich@hotmail.com>
2020-08-01 15:11:00 -0400

    pseudocode initial commit

---
+++ project1.js
@@ -0,0 +1,72 @@
+// let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q, A ];
+// let suits = [hearts, diamonds, spades, clubs];
+// let 
+// class GameRules {
+//      constructor() {
+//           this.maxValue = 21
+//           this.roundWin
+//           this.roundlose
+
+//      }
+//      checkWin(){
+//           if (playerCardValue >= dealerCardValue && playerCardValue >= 21);{
+//                console.log("you win")
+//           }
+//           checkLose(){
+//                if (playerCardValue < dealerCardValue && dealerCardValue <= 21){
+//                     console.log("you lose");
+//                }
+//           }
+
+//      }
+// }
+
+// class player {
+//      constructor(playerCash, playerCardValue, playerBet) {
+//           this.playerCash = 100
+//           this.playerCardValue = null
+//           this.playerBet = null
+//           this.hit = //+ 1 card
+//      }
+//      playerWin(playerCash) += this.playerBet;
+//      }
+//      playerLose(playerCash) -= this.playerBet;
+
+// }
+//      class dealer{
+//           constructor() {
+//                this.maxHit = 17
+
+          
+//           }
+//      }
+
+
+
+// // function shuffle()
+// // // use math.randon and math.floor to shuffle the array of cards.
+// create a new variable called deck
+
+// // function startBlackJack()
+// // // begins the game of blackjack
+
+// // dealCards(deck) //jquery
+// if dealButton = clicked display two player carda and two dealer cards via deck function. if pressed again pull another card for player. repeat until player pressed stayButton or goes over 21 //void fucntion
+
+// stayButton()
+// if playerCardValue < 21 && player pushes stayButton dealer hits until dealer card value is > 17
+
+// checkWin()
+
+// if checkWin === true
+// playerBet += playerBank
+
+// check lose === true
+// playerBet -= playerBank
+
+// // function playerBank()
+
+
+// loop back to beginning of the game but rentain player bank
+
+
