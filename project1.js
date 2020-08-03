62a9bbb73b617553d64155891ec3eb3e2a1cd24b
alex yanovich <alexyanovich@hotmail.com>
2020-08-03 18:56:31 -0400

    added an absurd amount of card values

--- project1.js
+++ project1.js
@@ -1,18 +1,3 @@
-// let cardValues = [
-//           ['2', 2, 'hearts'],
-//           ['3', 3, "hearts"],
-//           ['4', 4, 'hearts'],
-//           ['5', 5, 'hearts'],
-//           ['6', 6, 'hearts'],
-//           ['7', 7, 'hearts'],
-//           ['8', 8, 'hearts'],
-//           ['9', 9, 'hearts'],
-//           ['10', 10, 'hearts'],
-//           ['j', 10, 'hearts'],
-//           [K, 10],
-//           [Q, 10],
-//           [A, [1, 11]];
-
 class GameRules {
      constructor() {
           this.maxValue = 21
@@ -43,7 +28,7 @@ class deck {
 }
 
 
-class player {
+class Player {
      constructor() {
           this.playerCash = 100
           this.playerCardValue = 0
@@ -59,7 +44,7 @@ class player {
 }
 
 
-class dealer {
+class Dealer {
      constructor() {
           this.maxValue = 18
 
@@ -75,46 +60,50 @@ function shuffle() {
 }
 
 function startBlackJack() {
-     const player = new player();
-     const dealer = new dealer();
+     prompt("Are you ready to win some big time high roller money?");
+     const Player = new player();
+     const Dealer = new dealer();
      const playerHand = new deck(0);
      const dealerHand = new deck(0);
      const cardDeck = new deckSize(52);
-     const cardValues = [['2', 2], ['3', 3] , ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9], ['10', 10], ['J', 10], ['K', 10], ['Q', 10], ['A', [1, 11]]];
+     let cardValues = [
+          { name: "2 of Clubs", value: 2, filename: "2_of_clubs.svg" }, {name: "2 of diamonds", value :2, filename: "2_of_diamonds.svg"}, {name: "2 of hearts", value : 2, filename: "2_of_hearts.svg"}, {name: "2 of spades", value: 2, filename:"2_of_spades.svg"}, {name: "3 of clubs", value: 3,  filename:"3_of_clubs.svg"}, {name: "3 of diamonds", value: 3 , filename:"3 _of_diamonds.svg"}, {name: "3 of hearts", value: 3,  filename:"3_of_hearts.svg"},{name: "3 of spades", value: 3 , filename:"3_of_spades.svg"},{name: "4 of clubs", value: 4, filename:"4_of_clubs.svg"},{name: "4 of diamonds", value: 4, filename:"4_of_diamonds.svg"}, {name: "4 of hearts", value: 4, filename:"4_of_hearts.svg"},{name: "4 of spades", value: 4, filename:"4_of_spades.svg"},{name: "5 of clubs", value: 5, filename:"5_of_clubs.svg"}, {name: "5 of diamonds", value: 5, filename:"5_of_diamods.svg"}, {name: "5 of hearts", value: 5, filename:"5_of_hearts.svg"},{name: "5 of spades", value: 5, filename:"5_of_spades.svg"}, {name: "6 of clubs", value: 6, filename:"6_of_clubs.svg"},{name: "6 of diamonds", value: 6, filename:"6_of_diamonds.svg"},{name: "6 of hearts", value: 6, filename:"6_of_hearts.svg"},{name: "6 of spades", value: 6, filename:"6_of_spades.svg"},{name: "7 of clubs", value: 7, filename:"7_of_clubs.svg"},{name: "7 of diamonds", value: 7, filename:"7_of_diamonds.svg"},{name: "7 of hearts", value: 7, filename:"7_of hearts.svg"},{name: "7 of spades", value: 7, filename:"7_of_spades.svg"},{name: "8 of clubs", value: 8, filename:"8_of_clubs.svg"},{name: "8 of diamonds", value: 8, filename:"8_of_diamonds.svg"},{name: "8 of hearts", value: 8, filename:"8_of_hearts.svg"},{name: "8 of spades", value: 8, filename:"8_of_spades.svg"},{name: "9 of clubs", value: 9, filename:"9_of_clubs.svg"},{name: "9 of diamonds", value: 9, filename:"9_of_diamonds.svg"},{name: "9 of hearts", value: 9, filename:"9_of_hearts.svg"},{name: "9 of spades", value: 9, filename:"9"},{name: "10_of_clubs.svg", value: 10, filename:""},{name: "10 of diamonds", value: 10, filename:"10_of_diamonds.svg"},{name: "10 of hearts", value: 10, filename:"10_of_hearts.svg"}, {name:"10 of spades" , value: 10, filename:"10_of_spades.svg"},{name: "ace of clubs", value: 11, filename:"ace_of_clubs.svg"},{name: "ace of diamonds", value: 11, filename:"ace_of_diamonds.svg"},{name: "ace of hearts", value: 11, filename:"ace_of_hearts.svg"},{name: "ace of spades", value: 11, filename:"ace_of_spades.svg"},{name: "jack of clubs", value: 10, filename:"jack_of_clubs.svg"},{name: "jack of diamonds", value: 10, filename:"jack_of_diamonds.svg"},{name: "jack of hearts", value: 10, filename:"jack_of_hearts.svg"}, {name: "jack of spades", value: 10, filename:"jack_of_spades.svg"},{name: "king of clubs", value: 10, filename:"king_of_clubs.svg"}, {name: "king of diamonds", value: 10, filename:"king_of_diamonds.svg"}, {name: "king of hearts", value: 10, filename:"king_of_hearts.svg"}, {name: "king of spades", value: 10, filename:"king_of_spades.svg"},{name: "queen of clubs", value: 10, filename:"queen_of_clubs.svg"}, {name: "queen of diamonds", value: 10, filename:"queen_of_diamonds,svg"}, {name: "queen of hearts", value: 10, filename:"queen_of_hearts,svg"},
+        {name: "queen of spades", value: 10, filename:"queen_of_spades,svg"},
+     ]
+        ]
      const suitValue = ['ace', 'clubs', 'hearts', 'diamonds'];
      for(let i =0; i < suitValue.length; i++){
           for( let j = 0; j < cardValues.length; j++){
           // jquery add object with value of current suit and card value
           }
      }
+}
 
-     player.playerBet = prompt("enter your bet amount")
-     while (player.playerBet > player.playerCash || player.playeBet.NaN == true) {
+function betMoney(){
+     let playerBet = prompt("enter your bet amount");
+     while (player.playerBet > player.playerCash) {
           alert("you dont have anough money");
           player.playerBet = prompt("enter your bet amount");
      }
-
-
-     while (player.playerCardValue <= 21 && dealer.maxValue <= 18) {
-          //if player presses hit button draw  a new card/
-          // if player goes over 21  display you lose and subtract bet
-          // if player wins  display win and add bet
+}
+     function hitMe(){
+         
+     while (player.playerCardValue <= 21 && dealer.maxValue <= 18 && hitMe === true) {
+          console.log()
+          
      }
+     }
+     // $stayButton();{
+     // ('#stay").on("click", $stayButton())
+     //      if (player )
+     // } 
 
-     $stayButton(){
-     ('#stay").on("click", $stayButton())
-          if (player )
-} 
 
-
-     checkWin() 
+     checkWin(); 
      if (checkWin === true) {
           playerBet += playerBank
      }
-
-     checkLose()
+     checkLose();
      if (checkLose === true) {
           playerBet -= playerBank
      }
-}
-//     playerBank() =  .
